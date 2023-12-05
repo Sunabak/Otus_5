3dd4cc8c8fe41e362ccc360c7b3cc35d44a04ee4
Sunabak <150249259+Sunabak@users.noreply.github.com>
2023-12-05 16:36:10 +0300

    Update scr.sh

--- scr.sh
+++ scr.sh
@@ -13,7 +13,3 @@ yum install -y zfs
 modprobe zfs
 #install wget
 yum install -y wget
-sudo zpool create otus1 mirror /dev/sdb /dev/sdc
-sudo zpool create otus2 mirror /dev/sdd /dev/sde
-sudo zpool create otus3 mirror /dev/sdf /dev/sdg
-sudo zpool create otus4 mirror /dev/sdh /dev/sdi
